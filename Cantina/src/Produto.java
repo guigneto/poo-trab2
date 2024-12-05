ba1ac727915731fafa8f4c33f8f93dd9eadbd1cc
alliciarocha <155778524+alliciarocha@users.noreply.github.com>
2024-12-05 09:11:55 -0300

    Update Produto.java

--- Cantina/src/Produto.java
+++ Cantina/src/Produto.java
@@ -5,8 +5,8 @@ public class Produto {
     private int qnt;
     private double valor;
 
-    public Produto(String cod,String nome, int qnt, double valor) {
-        this.cod = cod;
+    public Produto(String nome, int qnt, double valor, Sistema s) {
+        this.cod = s.gerarCodigoProduto();
         this.qnt = qnt;
         this.nome = nome;
         this.valor = valor;
@@ -16,5 +16,19 @@ public class Produto {
         return "Nome do produto: " + this.nome + " - Quantidade: " + this.qnt + " - Valor: " + this.valor;
     }
 
-    public void retirarDeEstoque(int qnt){ this.qnt--; }
+    public void retirarDeEstoque(int qnt){
+        this.qnt--;
+    }
+
+    public String getCod() {
+        return cod;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
 }
