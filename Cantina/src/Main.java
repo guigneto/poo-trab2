504dfc00393201041faec414b152303a82e65bf2
Guilherme Gomes <guilhermeneto26@hotmail.com>
2025-02-14 10:06:39 -0300

    Update Main.java

--- Cantina/src/Main.java
+++ Cantina/src/Main.java
@@ -1,18 +1,19 @@
+import java.io.FileNotFoundException;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+
+
         Sistema s = new Sistema();
         Entrada e = new Entrada();
 
+        e.carregarDadosDoArquivo(s);
         try {
-            e.carregarDadosDoArquivo(s); // Tenta carregar os dados antes de abrir o menu
-            System.out.println("Dados carregados com sucesso!");
+            e.menu(s);
         } catch (Exception ex) {
-            System.out.println("Erro ao carregar dados: " + ex.getMessage());
-            ex.printStackTrace(); // Mostra o erro para depuração
+            ex.printStackTrace();
         }
-
-        e.menu(s); // Só chama o menu depois que os dados são carregados corretamente
     }
 }
